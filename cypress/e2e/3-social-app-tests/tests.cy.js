66121dc971c12c9788239d13e7f3eed3a5dd61a4
Lysaker-git <68820814+Lysaker-git@users.noreply.github.com>
2023-01-29 15:26:17 +0100

    added cyprus test for logout and failed login

--- cypress/e2e/3-social-app-tests/tests.cy.js
+++ cypress/e2e/3-social-app-tests/tests.cy.js
@@ -23,12 +23,12 @@ describe('login', () => {
         cy.get("#loginEmail").within(() => {
             cy.root().type(userMail, {delay: 0})
             cy.wait(1000)
-            cy.root().should('have.value', userMail)
+            expect(cy.root().should('have.value', userMail))
         })
         cy.get('#loginPassword').within(() => {
             cy.root().type(userPass, {delay: 0})
             cy.wait(1000)
-            cy.root().should('have.value', userPass)
+            expect(cy.root().should('have.value', userPass))
         })
         cy.get('#loginForm').within(() => {
             cy.get('[type="submit"]').click()
@@ -37,7 +37,27 @@ describe('login', () => {
             expect(loc.href).to.include('profile')
         })
     })
-    it('can log out with the log out button', () => {
+    it('can log out with the logout button', () => {
+        cy.get("#loginEmail").within(() => {
+            cy.root().type(userMail, {delay: 0})
+            cy.wait(1000)
+            expect(cy.root().should('have.value', userMail))
+        })
+        cy.get('#loginPassword').within(() => {
+            cy.root().type(userPass, {delay: 0})
+            cy.wait(1000)
+            expect(cy.root().should('have.value', userPass))
+        })
+        cy.get('#loginForm').within(() => {
+            cy.get('[type="submit"]').click()
+        })
+        cy.wait(2000)
+        cy.get('[data-auth="logout"]').click()
+        cy.location().should((loc) => {
+            expect(loc.href).to.not.include('profile')
+        })
+    })
+    it('can not log in with the wrong credentials', () => {
         cy.get("#loginEmail").within(() => {
             cy.root().type(wrongMail, {delay: 0})
             cy.wait(1000)
@@ -50,6 +70,15 @@ describe('login', () => {
         })
         cy.get('#loginForm').within(() => {
             cy.get('[type="submit"]').click()
+        })
+        cy.location().should((loc) => {
+            expect(loc.href).to.not.include('profile')
+        })
+        cy.on('window:alert', (content) => {
+            expect(content).include('Either your username was not found or your password is incorrect')
+        })
+        cy.then(() => {
+            expect(localStorage.getItem("token")).to.eq(null)
         })
     })
 })
